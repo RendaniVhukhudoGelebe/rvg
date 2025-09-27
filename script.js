e10b383d589c85d1e1ceb525c4af09a8ffe57d8b
RendaniVhukhudoGelebe <rendanivhukhudogelebe@gmail.com>
2025-09-27 16:39:21 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-// Smooth scroll for older browsers
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
+// Smooth scroll for links
+document.querySelectorAll('nav a').forEach(link => {
+  link.addEventListener('click', function(e) {
     e.preventDefault();
-    document
-      .querySelector(this.getAttribute('href'))
-      .scrollIntoView({ behavior: 'smooth' });
+    const section = document.querySelector(this.getAttribute('href'));
+    section.scrollIntoView({ behavior: 'smooth' });
   });
 });
